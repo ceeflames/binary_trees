88613c5202ca3433f78fce7c6a2c495e96da6d46
ceeflames <ceedizprodigy@gmail.com>
2023-11-02 00:55:18 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,9 +10,8 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 
