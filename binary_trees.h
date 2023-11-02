7fe68780678a7080ba8d84d13332f03e4585c219
ceeflames <ceedizprodigy@gmail.com>
2023-11-02 06:19:38 -0700

    binary_trees

--- binary_trees.h
+++ binary_trees.h
@@ -28,7 +28,6 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 /** structs */
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
