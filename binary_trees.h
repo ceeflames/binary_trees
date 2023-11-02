d038fe8eaa0b1a15c1df907f600ecc4a25b7e910
ceeflames <ceedizprodigy@gmail.com>
2023-11-02 01:04:24 -0700

    0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
-#ifndef BINARY_TREE
-#define BINARY_TREE
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,13 +19,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 
 /** structs */
 typedef struct binary_tree_s binary_tree_t;
@@ -34,5 +35,6 @@ typedef struct binary_tree_s heap_t;
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
