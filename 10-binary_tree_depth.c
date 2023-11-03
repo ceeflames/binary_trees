6ef6d8cdc9fa2eaf244c1127e5f4170d30cd64c3
ceeflames <ceedizprodigy@gmail.com>
2023-11-03 10:02:35 -0700

    10depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * find_tree_depth - function that measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
+ * @num: measured tree depth
+ *
+ * Return: 0
+ */
+size_t find_tree_depth(const binary_tree_t *tree, size_t num)
+{
+	if (tree && !tree->parent)
+		return (num);
+	if (tree)
+		return (find_tree_depth(tree->parent, num + 1 ));
+	return (num);
+}
+
+/**
  * binary_tree_depth - function that measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
  *
@@ -8,5 +24,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return (binary_tree_depth(tree, 0));
+	return (find_tree_depth(tree, 0));
 }
