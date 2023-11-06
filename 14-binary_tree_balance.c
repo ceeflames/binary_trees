9bcdb08c28b55e75fe7cbc952eb93393bbdf965b
ceeflames <ceedizprodigy@gmail.com>
2023-11-06 09:39:44 -0800

    14binary

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_balance - function that measures the balance
+ * factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * the balance factor
+ *
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	binary_tree_t *tmp = tree ? tree->left : NULL;
+
+	if (tree)
+	{
+		if (tree->left && tree->right)
+			return(binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		else
+			return (tmp ? binary_tree_height(tree) - 0 : 0 - binary_tree_height(tree));
+	}
+	return (0);
+}
